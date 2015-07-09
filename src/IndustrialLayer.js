7d9116f639bdc0156040d3c743a63f133b70ba74
Rune Holmgren <Raane.Holm@gmail.com>
2015-07-09 23:39:09 +0200

    Added the model and  lighting for the industrial scene.

--- src/IndustrialLayer.js
+++ src/IndustrialLayer.js
@@ -2,31 +2,14 @@
  * @constructor
  */
 
-function pseudorand() {
-    var max = Math.pow(2, 32),
-        seed;
-    return {
-        setSeed : function(val) {
-            seed = val || Math.round(Math.random() * max);
-          },
-        getSeed : function() {
-                      return seed;
-                  },
-        rand : function() {
-                   //creates randomness...somehow...
-                       seed += (seed * seed) | 5;
-                   // Shift off bits, discarding the sign. Discarding the sign is
-                   // important because OR w/ 5 can give us + or - numbers.
-                   return (seed >>> 32) / max;
-       }
-    };
-};
-
 function IndustrialLayer(layer) {
     Math.seedrandom("run");
     this.layer = layer;
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(45, 16 / 9, .01, 1000);
+    //this.camera = new THREE.PerspectiveCamera(45, 16 / 9, .01, 1000);
+
+    this.cameraController = new CameraController(layer.type);
+    this.camera = this.cameraController.camera;
     this.camera.position.x = 0;
     this.camera.position.y = 0;
     this.camera.position.z = 40;
@@ -34,34 +17,50 @@ function IndustrialLayer(layer) {
     this.renderPass = new THREE.RenderPass(this.scene, this.camera);
     this.fontLoaded = false;
 
-    this.scene.fog = new THREE.Fog(0xedc9af, 50, 90);
+    this.scene.fog = new THREE.Fog(0x7B665A, 0, 200);
 
     loader = new THREE.JSONLoader();
-
-    var rand = pseudorand();
-    rand.setSeed();
-    for(i=0;i<1000;i++) {
-        var sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 5, 5), new THREE.MeshNormalMaterial());
-        sphere.position.set(
-                rand.rand()*120-5,
-                rand.rand()*100-5,
-                rand.rand()*120-5
-                );
-        sphere.overdraw = true;
-        this.scene.add(sphere);
-    }
+    
+    this.initLights();
     this.initNinjadev();
+}
+
+IndustrialLayer.prototype.initLights= function() {
+
+    this.ambientLight = new THREE.AmbientLight(0x555555);
+    this.light1 = new THREE.PointLight(0x992222, 20.0, 100);
+    this.light2 = new THREE.PointLight(0x992222, 20.0, 100);
+    this.light3 = new THREE.PointLight(0x992222, 20.0, 100);
+    this.light4 = new THREE.PointLight(0x992222, 20.0, 100);
+    //this.light5 = new THREE.PointLight(0xFFEAC3, .3);
+    this.WATER_CENTER_X = 0;
+    this.WATER_CENTER_Z = 10;
+    this.directionalLight = new THREE.DirectionalLight( 0x999999, 3 );
+    this.light1.position.set(-47.54,69.36,-87.07);
+    this.light2.position.set(40.17,70.56,90);
+    this.light3.position.set(45.2,70.95,-86.78);
+    this.light4.position.set(-41.53,70.77,90);
+    this.directionalLight.position = new THREE.Vector3(0,0,70);
+    this.scene.add(this.ambientLight);
+    this.scene.add(this.light1);
+    this.scene.add(this.light2);
+    this.scene.add(this.light3);
+    this.scene.add(this.light4);
+    //this.scene.add(this.light5);
+    this.scene.add(this.directionalLight);
 }
 
 IndustrialLayer.prototype.initNinjadev = function() {
     this.shoutouts;
+    this.concrete;
     var that = this;
-    var shoutoutsMaterial = new THREE.MeshNormalMaterial({
-        color: 0x105107,
-        side: THREE.DoubleSide
+    var shoutoutsMaterial = new THREE.MeshPhongMaterial({
+        color: 0x5e1a07,
+        side: THREE.DoubleSide,
+        metal: true
     });
-    var holdkjeftMaterial= new THREE.MeshNormalMaterial({
-        color: 0xff3322,
+    var concreteMaterial = new THREE.MeshLambertMaterial({
+        color: 0x555555,
         side: THREE.DoubleSide
     });
 
@@ -92,23 +91,19 @@ IndustrialLayer.prototype.initNinjadev = function() {
                 }
             });
             group.add(object);
-            loadedCounter++;
-            if (loadedCounter >= numComponents) {
-                addObjects();
-            }
+            addObjects();
         });
     };
+
     var addObjects2 = function() {
 
         var shoutouts = group2.clone();
-        shoutouts.position.x = 300;
-        shoutouts.position.y = 58;
-        shoutouts.position.z = 96;
-        shoutouts.rotation.x = Math.PI/2;
+        shoutouts.position.x = 0;
+        shoutouts.position.y = 0;
+        shoutouts.position.z = 1;
         shoutouts.rotation.y = 0;
-        shoutouts.rotation.z = -Math.PI/2;
         shoutouts.scale.set(6,6,6);
-        that.shoutouts = shoutouts;
+        that.concrete = shoutouts;
         that.scene.add(shoutouts);
     };
 
@@ -122,26 +117,32 @@ IndustrialLayer.prototype.initNinjadev = function() {
                 }
             });
             group2.add(object);
-            loadedCounter++;
-            if (loadedCounter >= numComponents) {
-                addObjects2(object);
-            }
+            addObjects2();
         });
     };
     var prefix = 'res/objects/';
-    loadObject(prefix + 'shoutouts.obj', shoutoutsMaterial);
-    loadObject2(prefix + 'holdkjeft.obj', holdkjeftMaterial);
+    loadObject(prefix + 'industrial-scene-rust.obj', shoutoutsMaterial);
+    loadObject2(prefix + 'industrial-scene-concrete.obj', concreteMaterial);
     Loader.start(function(){}, function(text) {console.log(text);});
 };
 
 IndustrialLayer.prototype.update = function(frame, relativeFrame) {
-    // CAMERA MOVEMENT
+
+    this.cameraController.updateCamera(relativeFrame);
+
     var shout1 = 161;
     var shout2 = 371;
     var shout3 = 581;
     var shout4 = 791;
     var shout5 = 1001;
     var shout6 = 1211;
+
+    var intensity = 20*(0.5+0.5*Math.cos(relativeFrame*Math.PI/25.7/4));
+    this.light1.intensity = intensity;
+    this.light2.intensity = intensity;
+    this.light3.intensity = intensity;
+    this.light4.intensity = intensity;
+/*
     var current_x = lerp(10,50,relativeFrame/shout4) + lerp(0,50,(relativeFrame-(shout5-120))/(shout6-shout5)) + lerp(0,1115,(relativeFrame-(shout5+60))/(shout6-shout5));
     this.camera.position.x = current_x;
     this.camera.position.y = 30 + lerp(-10,0,relativeFrame/100);
@@ -183,6 +184,7 @@ IndustrialLayer.prototype.update = function(frame, relativeFrame) {
     if(relativeFrame > 1103) {
         this.camera.position.set(323, 60, 90);
     }
+    */
 }
 
 IndustrialLayer.prototype.getEffectComposerPass = function() {
