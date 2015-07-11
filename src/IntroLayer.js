697a5502a3b87b66f71042884393ae9f3b1bce3d
Rune Holmgren <Raane.Holm@gmail.com>
2015-07-11 04:00:18 +0200

    Added a simple introscene

--- src/IntroLayer.js
+++ src/IntroLayer.js
@@ -2,26 +2,6 @@
  * @constructor
  */
 
-function pseudorand() {
-    var max = Math.pow(2, 32),
-        seed;
-    return {
-        setSeed : function(val) {
-            seed = val || Math.round(Math.random() * max);
-          },
-        getSeed : function() {
-                      return seed;
-                  },
-        rand : function() {
-                   //creates randomness...somehow...
-                       seed += (seed * seed) | 5;
-                   // Shift off bits, discarding the sign. Discarding the sign is
-                   // important because OR w/ 5 can give us + or - numbers.
-                   return (seed >>> 32) / max;
-       }
-    };
-};
-
 function IntroLayer(layer) {
     Math.seedrandom("run");
     this.layer = layer;
@@ -29,57 +9,71 @@ function IntroLayer(layer) {
     this.camera = new THREE.PerspectiveCamera(45, 16 / 9, .01, 1000);
     this.camera.position.x = 0;
     this.camera.position.y = 0;
-    this.camera.position.z = 40;
-    this.camera.lookAt(new THREE.Vector3(6,4,0));
+    this.camera.position.z = 37;
+    this.camera.lookAt(new THREE.Vector3(0,0,0));
     this.renderPass = new THREE.RenderPass(this.scene, this.camera);
     this.fontLoaded = false;
 
-    this.scene.fog = new THREE.Fog(0xedc9af, 50, 90);
+    this.toruses = [];
+    this.bars = [];
+    for(var i=0;i<18;i++) this.add_torus();
+    this.add_run_bar(1,3,-3,0);
+    this.add_run_bar(3,1,-2,1);
+    this.add_run_bar(1,3,-1,0);
+    this.add_run_bar(3,1,-0,-1);
+    this.add_run_bar(1,3,1,0);
+    this.add_run_bar(3,1,2,1);
+    this.add_run_bar(1,3,3,0);
+    this.add_run_bar(1,3,1000,0);
 
-    loader = new THREE.JSONLoader();
-
-    var rand = pseudorand();
-    rand.setSeed();
-    for(i=0;i<1000;i++) {
-        var sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 5, 5), new THREE.MeshNormalMaterial());
-        sphere.position.set(
-                rand.rand()*120-5,
-                rand.rand()*100-5,
-                rand.rand()*120-5
-                );
-        sphere.overdraw = true;
-        this.scene.add(sphere);
-    }
-    this.initNinjadev();
+    this.initHoldKjeft();
 }
 
-IntroLayer.prototype.initNinjadev = function() {
+IntroLayer.prototype.add_torus = function() {
+    var geometry = new THREE.TorusGeometry( 10, 2, 6, 30 );
+    var material = new THREE.MeshBasicMaterial( { color: 0xFFFF66 } );
+    var torus = new THREE.Mesh( geometry, material );
+    this.scene.add( torus );
+    this.toruses.push(torus);
+}
+
+IntroLayer.prototype.add_run_bar = function(x,y,dx,dy) {
+    var geometry = new THREE.BoxGeometry( x, y, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x009933 } );
+    var torus = new THREE.Mesh( geometry, material );
+    torus.position.set(dx,dy,0);
+    this.scene.add( torus );
+    this.bars.push(torus);
+}
+
+IntroLayer.prototype.initHoldKjeft = function() {
     this.shoutouts;
     var that = this;
-    var shoutoutsMaterial = new THREE.MeshNormalMaterial({
-        color: 0x105107,
-        side: THREE.DoubleSide
-    });
-    var holdkjeftMaterial= new THREE.MeshNormalMaterial({
-        color: 0xff3322,
+    var holdkjeftMaterial= new THREE.MeshLambertMaterial({
+        color: 0x009933,
         side: THREE.DoubleSide
     });
 
     var numComponents = 1;
     var loadedCounter = 0;
     var group = new THREE.Object3D();
-    var group2 = new THREE.Object3D();
 
     var addObjects = function() {
 
         var shoutouts = group.clone();
-        shoutouts.position.x = 0;
-        shoutouts.position.y = 0;
-        shoutouts.position.z = 1;
+        shoutouts.position.x = -12.2;
+        shoutouts.position.y = -24;
+        shoutouts.position.z = 0;
+        shoutouts.rotation.x = Math.PI/2;
         shoutouts.rotation.y = 0;
-        shoutouts.scale.set(6,6,6);
+        shoutouts.rotation.z = 0;
+        shoutouts.scale.set(9,9,9);
         that.shoutouts = shoutouts;
         that.scene.add(shoutouts);
+
+        var light1 = new THREE.PointLight(0x992222, 20.0, 100);
+        light1.position.set(20,-24,40);
+        that.scene.add(light1);
     };
 
     var loadObject = function (objPath, material) {
@@ -94,95 +88,31 @@ IntroLayer.prototype.initNinjadev = function() {
             group.add(object);
             loadedCounter++;
             if (loadedCounter >= numComponents) {
-                addObjects();
-            }
-        });
-    };
-    var addObjects2 = function() {
-
-        var shoutouts = group2.clone();
-        shoutouts.position.x = 300;
-        shoutouts.position.y = 58;
-        shoutouts.position.z = 96;
-        shoutouts.rotation.x = Math.PI/2;
-        shoutouts.rotation.y = 0;
-        shoutouts.rotation.z = -Math.PI/2;
-        shoutouts.scale.set(6,6,6);
-        that.shoutouts = shoutouts;
-        that.scene.add(shoutouts);
-    };
-
-    var loadObject2 = function (objPath, material) {
-        var objLoader = new THREE.OBJLoader();
-        Loader.loadAjax(objPath, function(text) {
-            var object = objLoader.parse(text);
-            object.traverse(function(child) {
-                if (child instanceof THREE.Mesh) {
-                    child.material = material;
-                }
-            });
-            group2.add(object);
-            loadedCounter++;
-            if (loadedCounter >= numComponents) {
-                addObjects2(object);
+                addObjects(object);
             }
         });
     };
     var prefix = 'res/objects/';
-    loadObject(prefix + 'shoutouts.obj', shoutoutsMaterial);
-    loadObject2(prefix + 'holdkjeft.obj', holdkjeftMaterial);
+    loadObject(prefix + 'holdkjeft.obj', holdkjeftMaterial);
     Loader.start(function(){}, function(text) {console.log(text);});
-};
+}
 
 IntroLayer.prototype.update = function(frame, relativeFrame) {
-    // CAMERA MOVEMENT
-    var shout1 = 161;
-    var shout2 = 371;
-    var shout3 = 581;
-    var shout4 = 791;
-    var shout5 = 1001;
-    var shout6 = 1211;
-    var current_x = lerp(10,50,relativeFrame/shout4) + lerp(0,50,(relativeFrame-(shout5-120))/(shout6-shout5)) + lerp(0,1115,(relativeFrame-(shout5+60))/(shout6-shout5));
-    this.camera.position.x = current_x;
-    this.camera.position.y = 30 + lerp(-10,0,relativeFrame/100);
-    this.camera.position.z = lerp(0,75,relativeFrame/shout5) + lerp(0,20,(relativeFrame-shout4)/(shout6-shout4));
-    if(relativeFrame>0 && relativeFrame < shout1) {
-        //smoothstep(start, stop, progress);
-        this.camera.lookAt(new THREE.Vector3(
-                    current_x/1.3,
-                    0,
-                    smoothstep(-15,0,(relativeFrame-0)/(shout1-0))
-        ));
-    } else if(relativeFrame < shout2) {
-        this.camera.lookAt(new THREE.Vector3(
-                    current_x/1.3,
-                    0,
-                    smoothstep(0,15,(relativeFrame-shout1)/(shout3-shout2))
-        ));
-    } else if(relativeFrame < shout3) {
-        this.camera.lookAt(new THREE.Vector3(
-                    current_x/1.3,
-                    0,
-                    smoothstep(15,30,(relativeFrame-shout2)/(shout4-shout3))
-        ));
-    } else if(relativeFrame < shout4) {
-        this.camera.lookAt(new THREE.Vector3(
-                    current_x/1.3,
-                    0,
-                    smoothstep(30,50,(relativeFrame-shout3)/(shout5-shout4))
-        ));
-    } else {
-        this.camera.lookAt(new THREE.Vector3(
-                    current_x/1.3 + smoothstep(0,11,(relativeFrame-shout4)/(shout6-shout5)),
-                    0,
-                    smoothstep(50,80,(relativeFrame-shout4)/(shout6-shout5))
-        ));
-        this.camera.position.y = smoothstep(30,60,(relativeFrame-shout4)/(shout6-shout5))
+    for(var i=0;i<this.toruses.length;i++) {
+        this.toruses[i].position.z = (relativeFrame-i*2*25.7)/5;
     }
+    for(var i=0;i<this.bars.length;i++) {
+        this.bars[i].position.z = Math.min(29,(relativeFrame-i*4*25.7)/5);
+    }
+    this.bars[0].position.x = smoothstep(-3,-4.5,(relativeFrame-877)/100);
+    this.bars[1].position.x = smoothstep(-2,-3.5,(relativeFrame-877)/100);
+    this.bars[7].position.x = relativeFrame>877?1:1000;
+    this.bars[4].position.x = smoothstep(1,2.5,(relativeFrame-877)/100);
+    this.bars[5].position.x = smoothstep(2,3.5,(relativeFrame-877)/100);
+    this.bars[6].position.x = smoothstep(3,4.5,(relativeFrame-877)/100);
 
-    if(relativeFrame > 1103) {
-        this.camera.position.set(323, 60, 90);
-    }
+
+    this.camera.position.y = smoothstep(0,-24,(relativeFrame-1153)/12);
 }
 
 IntroLayer.prototype.getEffectComposerPass = function() {
